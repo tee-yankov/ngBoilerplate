ac796c84827724f7b5b93916383fe1a3c754e341
Theodore Yankov <venoma333@gmail.com>
2015-01-29 10:38:57 +0200

    initial structure additions

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,24 @@
-var gulp = require('gulp'),
-    inject = require('gulp-inject'),
+var gulp = require('gulp'), // https://github.com/gulpjs/gulp
+    inject = require('gulp-inject'), // https://github.com/klei/gulp-inject
     angularFilesort = require('gulp-angular-filesort'); // https://github.com/klei/gulp-angular-filesort
 
 
 var assets = {
-    js: ['./client/**/*.js'],
+    js: ['./client/**/*.js', './client/*.js'],
     scss: ['./client/**/*.scss'],
     html: ['./client/**/*.html', 'client/*.html']
 };
 
+// Inject javascript files for development into index.html
 gulp.task('inject:js', function() {
-    var target = gulp.src('public/index.html'),
-        sources = gulp.src(assets.js);
+    var target = gulp.src('client/index.html'),
+        sources = gulp.src(assets.js).pipe(angularFilesort());
 
-    return target.pipe(inject(sources))
+    return target.pipe(inject(sources, {
+        ignorePath: 'client'
+    }))
     .pipe(gulp.dest('./client'));
 });
+
+// Start the server and run startup taska
+gulp.task('server', ['inject:js']);
